aeab80c583cbeb749f923550de207c1c3f15c7b5
Sandro Papais <papais.sandro@gmail.com>
2020-01-28 23:06:14 -0500

    Removed namespaces and added image denoising.

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,10 +9,9 @@ Longer description here...
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
-using namespace cv;
-using namespace std;
+//using namespace cv;
 
-void lanefinding(Mat img);
+void lanefinding(cv::Mat &img);
 
 int main(void)
 {
@@ -23,50 +22,48 @@ int main(void)
     {
     
         // Get all images
-        vector<String> files;
-        glob("/home/sandro/Dev/lane_finding/test_images/*.jpg", files);
-        vector<Mat> images;
+        std::vector<std::string> files;
+        cv::glob("/home/sandro/Dev/lane_finding/test_images/*.jpg", files);
         size_t count = files.size(); // number of images in folder     
         
         for (size_t i=0; i<count; i++)
         {              
-                                                        
             // Read image      
-            Mat img = imread(files[i]);
+            cv::Mat img = cv::imread(files[i]);
             
             // Error handling
             if (img.empty())
             {
-                cout << "Could not open the image" << endl;
-                cin.get();
+                std::cout << "Could not open the image" << std::endl;
+                std::cin.get();
                 return -1;
             }
                 
             lanefinding(img);
             
-            waitKey(0);
+            cv::waitKey(0);
             
         }
     }
     else
     {
         // Open video file
-        VideoCapture cap("test_video/solidWhiteRight.mp4");
+        cv::VideoCapture cap("test_video/solidWhiteRight.mp4");
         
         // Error handling
         if (cap.isOpened() == false)
         {
-            cout << "Cannot open video file" << endl;
-            cin.get();
+            std::cout << "Cannot open video file" << std::endl;
+            std::cin.get();
             return -1;
         }
         
         // Initialize variables
-        Mat img;
+        cv::Mat img;
         bool readSuccess = true;
         
         // skip to 8 seconds
-        cap.set(CAP_PROP_POS_MSEC, 8000); 
+        cap.set(cv::CAP_PROP_POS_MSEC, 8000); 
         
         while (readSuccess)
         {
@@ -74,7 +71,7 @@ int main(void)
             
             lanefinding(img);
             
-            waitKey(0);
+            cv::waitKey(0);
 
         }
         
--- src/lane_detector.cpp
+++ src/lane_detector.cpp
@@ -4,71 +4,73 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
  
-using namespace cv;
-using namespace std;
-
-void Mask(Mat img, Mat *imgMasked, int w, int h);
-void lanefinding(Mat img);
+void Mask(cv::Mat img, cv::Mat &imgMasked, int w, int h);
+void lanefinding(cv::Mat &img);
 
 
-void lanefinding(Mat img){
+void lanefinding(cv::Mat &img){
             // Get image dimensions
             int w = img.size().width;
             int h = img.size().height;
             
+            // Denoise image
+            cv::Mat imgDenoised;
+            cv::GaussianBlur(img, imgDenoised, cv::Size(3, 3), 0, 0);           
+            
             // Gray scale image
-            Mat imgGray;
-            cvtColor(img,imgGray,COLOR_RGB2GRAY,0);
+            cv::Mat imgGray;
+            cvtColor(imgDenoised,imgGray,cv::COLOR_RGB2GRAY,0);
             
             // Canny edge detection
-            Mat imgCanny;
-            Canny(imgGray,imgCanny,50,200);
+            cv::Mat imgCanny;
+            cv::Canny(imgGray,imgCanny,50,200);
             
             // Create masked image
-            Mat imgMasked;
-            Mask(imgCanny, &imgMasked, w, h);
+            cv::Mat imgMasked;
+            Mask(imgCanny, imgMasked, w, h);
             
             // Probabilistic line transform
-            vector<Vec4i> linesP;
-            HoughLinesP(imgMasked, linesP, 1, CV_PI/180, 50, 50, 10);
+            std::vector<cv::Vec4i> linesP;
+            cv::HoughLinesP(imgMasked, linesP, 1, CV_PI/180, 50, 50, 10);
             
             // Draw lines
-            Mat imgLines;
-            cvtColor(imgMasked, imgLines, COLOR_GRAY2RGB);
+            cv::Mat imgLines;
+            cv::cvtColor(imgMasked, imgLines, cv::COLOR_GRAY2RGB);
             for(size_t i=0; i<linesP.size(); i++)
             {
-                Vec4i l = linesP[i];
-                line(imgLines, Point(l[0],l[1]), Point(l[2],l[3]), Scalar(0,0,255), 3, LINE_AA);
+                cv::Vec4i l = linesP[i];
+                line(imgLines, cv::Point(l[0],l[1]), cv::Point(l[2],l[3]), cv::Scalar(0,0,255), 3, cv::LINE_AA);
             }
         
             // Create windows and show image    
-            namedWindow("Raw");
+            cv::namedWindow("Raw");
             imshow("Raw", img);
-            namedWindow("Edge Detection");
+            cv::namedWindow("Edge Detection");
             imshow("Edge Detection", imgCanny);
-            namedWindow("Masked");
+            cv::namedWindow("Masked");
             imshow("Masked", imgMasked);
-            namedWindow("Lines");
+            cv::namedWindow("Lines");
             imshow("Lines", imgLines);
 }
 
-void Mask(Mat img, Mat *imgMasked, int w, int h){
+void Mask(cv::Mat img, cv::Mat &imgMasked, int w, int h){
     
     // Initialize mask
-    Mat imgMask = Mat::zeros(h, w, CV_8UC1);
+    cv::Mat imgMask = cv::Mat::zeros(h, w, CV_8UC1);
 
     // Define edge points of polygon for image mask
-    Point vertices[1][3]; 
-    vertices[0][0] = Point(0, h);
-    vertices[0][1] = Point(w/2, h/2);
-    vertices[0][2] = Point(w, h);
+    cv::Point vertices[1][3]; 
+    vertices[0][0] = cv::Point(0, h);
+    vertices[0][1] = cv::Point(w/2, h/2);
+    vertices[0][2] = cv::Point(w, h);
     
     // Create image mask
-    int lineType = LINE_8;
-    const Point* ppt[1] = {vertices[0]};
+    int lineType = cv::LINE_8;
+    const cv::Point* ppt[1] = {vertices[0]};
     int npt[] = {3};
-    fillPoly(imgMask, ppt, npt, 1, Scalar(255,255,255), lineType);
+    cv::fillPoly(imgMask, ppt, npt, 1, cv::
+    Scalar(255,255,255), lineType);
     
     // Create masked image
-    bitwise_and(img, img, *imgMasked, imgMask);
+    cv::bitwise_and(img, img, imgMasked, imgMask);
 }
