910bef6110f3fc58ebdabbed2ff1ca18c22dbf8c
Sandro Papais <papais.sandro@gmail.com>
2020-02-02 23:55:57 -0500

    Added linear regression on segmented lane lines.

--- src/lane_detector.cpp
+++ src/lane_detector.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <opencv2/core/utility.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+//#include <opencv2/core/utility.hpp>
+//#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/imgproc.hpp>
+//#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
  
 void Mask(cv::Mat img, cv::Mat &imgMasked, int w, int h);
 void lanefinding(cv::Mat &img);
@@ -47,32 +48,66 @@ void lanefinding(cv::Mat &img){
                 x1 = linesP[i][2];
                 y1 = linesP[i][3];
                 slopes.push_back((y1-y0)/(x1-x0));
-                std::cout << linesP[i] << std::endl;
-                std::cout << slopes[i] << std::endl;
+                //std::cout << linesP[i] << std::endl;
+                //std::cout << slopes[i] << std::endl;
                 if (slopes[i] > slopeThresh)
                 {
                     linesL.push_back(linesP[i]);
-                    std::cout << "left line"<< std::endl;
+                    //std::cout << "left line"<< std::endl;
                 }
                 else if (slopes[i] < -slopeThresh)
                 {
                     linesR.push_back(linesP[i]);
-                    std::cout << "right line"<< std::endl;
+                    //std::cout << "right line"<< std::endl;
                 }
                 else
                 {
-                    std::cout << "line discarded"<< std::endl;
+                    //std::cout << "line discarded"<< std::endl;
                 }
             }
             
-            // Regression of lines
+            // Regression of left line
+            cv::Vec4i l;
+            std::vector<cv::Point> pointsL; 
+            cv::Vec4d lineLfit;
+            double left_m;
+            cv::Point left_b[1][1];
+
+            for(size_t i=0; i<linesL.size(); i++)
+            {                
+                l = linesL[i];
+                pointsL.push_back(cv::Point(l[0], l[1]));
+                pointsL.push_back(cv::Point(l[2], l[3]));
+            }            
+            cv::fitLine(pointsL, lineLfit, cv::DIST_L2, 0, 0.01, 0.01);
+            left_m = lineLfit[1]/lineLfit[0];
+            left_b[0][0] = cv::Point(lineLfit[2], lineLfit[3]);
             
-
-
+            // Regression of right line
+            std::vector<cv::Point> pointsR;
+            cv::Vec4d lineRfit;
+            double right_m;
+            cv::Point right_b[1][1];
+            
+            for(size_t i=0; i<linesR.size(); i++)
+            {                
+                l = linesR[i];
+                pointsR.push_back(cv::Point(l[0], l[1]));
+                pointsR.push_back(cv::Point(l[2], l[3]));
+            }
+            cv::fitLine(pointsR, lineRfit, cv::DIST_L2, 0, 0.01, 0.01);            
+            right_m = lineRfit[1]/lineRfit[0];
+            right_b[0][0] = cv::Point(lineRfit[2],lineRfit[3]);
+            
+            // Create left and right line from regression
+            std::cout << left_m << "," << left_b[0][0] << std::endl;
+            std::cout << right_m << "," << right_b[0][0] << std::endl;
+            
             // Draw lines
             cv::Mat imgLines;
-            cv::Vec4i l;
             cv::cvtColor(imgMasked, imgLines, cv::COLOR_GRAY2RGB);
+            
+            imgLines = img;
             for(size_t i=0; i<linesP.size(); i++)
             {
                 l = linesP[i];
@@ -88,6 +123,12 @@ void lanefinding(cv::Mat &img){
                 l = linesR[i];
                 line(imgLines, cv::Point(l[0],l[1]), cv::Point(l[2],l[3]), cv::Scalar(255,0,0), 3, cv::LINE_AA);
             }
+            //std::cout << lineRfit << std::endl;
+            //l = lineRfit[0];
+            //line(imgLines, cv::Point(l[0],l[1]), cv::Point(l[2],l[3]), cv::Scalar(0,255,0), 3, cv::LINE_AA);
+            //l = lineRfit[0];
+            //line(imgLines, cv::Point(l[0],l[1]), cv::Point(l[2],l[3]), cv::Scalar(0,255,0), 3, cv::LINE_AA);
+            
         
             // Create windows and show image    
             cv::namedWindow("Raw");
